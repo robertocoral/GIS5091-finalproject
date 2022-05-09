37b9b571833e5ee6a6ed96334abc0a37cfa20375
Roberto Coral <33666303+robertocoral@users.noreply.github.com>
2022-05-09 17:29:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,94 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-        var map = new Map({
-          basemap: "topo-vector"
-        });
+require([
+      "esri/config",
+      "esri/Map",
+      "esri/layers/FeatureLayer",
+      "esri/views/MapView",
+      "esri/widgets/Legend",
+      "dojo/domReady!"
+    ], function(esriConfig, Map, FeatureLayer, MapView, Legend) {
+  
+    esriConfig.apiKey = "AAPK46f57464803c4f51ad1e192af78e31f82ceOIxvtHx2AdmAvfFMB5rMhnid1IlMzs_i3su5KhQVNLfG1_P84UEJ3h1YtpCu6";
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 4,
-          center: [15, 65] // longitude, latitude
-        });
-      });
+    var map = new Map({
+        basemap: "arcgis-terrain"
+    });
+    
+    var view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center:[-101, 41],
+        zoom: 4
+    });
+
+    var template = { // autocasts as new PopupTemplate()
+        title: "City: {cbsatitle}",
+        content: [{
+            type: "fields",
+            fieldInfos: [{
+                fieldName: "posts_jan2 ",
+                label: "Job Postings (January 2020): ",
+                visible: true
+            }]
+         }]
+    };
+  
+    const defaultSym = {
+        type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+        color: "red"
+    };
+ 
+    const renderer = {
+      type: "simple", // autocasts as new SimpleRenderer()
+      symbol: defaultSym,
+      visualVariables: [
+        {
+          type: "size",
+          field: "posts_jan2",
+          // normalizationField: "TOTPOP_CY",
+          legendOptions: {
+            title: "Count"
+          },
+          stops: [
+            {
+              value: 5000,
+              size: 5,
+              label: "<5,000"
+            },
+            {
+              value: 50000,
+              size: 15,
+              label: "<50,000"
+            },
+            {
+              value: 100000,
+              size: 25,
+              label: "<100,000"
+            },
+            {
+              value: 500000,
+              size: 40,
+              label: "<500,000"
+            }
+          ]
+        }
+      ]
+    };
+  
+    var featureLayer = new FeatureLayer({
+        url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/adecce/FeatureServer",
+        outFields: ["*"],
+       title: "Job Postings in January 2020",
+        popupTemplate: template,
+        renderer:renderer
+    });
+  
+    map.add(featureLayer);
+  
+    view.ui.add(
+        new Legend({
+        view: view
+        }),
+        "top-right"
+    );
+
+});
